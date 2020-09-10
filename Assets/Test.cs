4adcd60c5e7843a3195fb684b7ad240229e014a8
kinoko0228 <70248669+kinoko0228@users.noreply.github.com>
2020-09-11 00:25:30 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss
+{
+    private int mp = 53;
+    
+    public void Magic()
+    {
+        this.mp -= 5;
+        if(mp >= 0)
+        {
+            Debug.Log("魔法攻撃をした。残りMPは"+mp);
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない");
+        }
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = new int[5];
+
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 30;
+        array[3] = 40;
+        array[4] = 50;
+
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for(int a = 4; a >= 0; a--)
+        {
+            Debug.Log(array[a]);
+        }
+        Boss magic = new Boss ();
+        for(int i = 0; i < 11; i++)
+        {
+            magic.Magic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
